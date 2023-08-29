8898035574feb0d6901ba89106fdaafeb12a6674
pidi3000 <76975863+pidi3000@users.noreply.github.com>
2023-08-30 00:12:09 +0200

    Nightly (#2)
    * Update config.py
    
    * Update Publish-Docker-image.yml
    
    * Update actions_test.yml
    
    * Update actions_test.yml
    
    * Update actions_test.yml
    
    * Update actions_test.yml
    
    * Update actions_test.yml
    
    * Update actions_test.yml

--- music_feed/_data/config.py
+++ music_feed/_data/config.py
@@ -7,13 +7,12 @@ db_path = str(basedir.joinpath("data/database.db").absolute())
 
 
 class Config:
-    SECRET_KEY = 'REPLACE ME - this value is here as a placeholder.'  # os.environ.get('SECRET_KEY')
-    
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'REPLACE ME - this value is here as a placeholder.'
+
     SQLALCHEMY_DATABASE_URI = \
         os.environ.get('DATABASE_URI')\
         or 'sqlite:///' + db_path
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
     FEED_UPLOADS_PER_PAGE = 4*20
-
