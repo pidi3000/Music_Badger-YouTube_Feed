924082bea775142fc57b47a8e7407c353f03fbef
pidi3000 <pidi3000@live.com>
2024-05-31 22:06:15 +0200

    add __init__.py for data module

--- music_feed/youtube/data/__init__.py
+++ music_feed/youtube/data/__init__.py
@@ -1,2 +1,8 @@
 
-from .data import *
+from .subscriptions import *
+from .channel import *
+
+
+def update_Uploads():
+    from music_feed.youtube.data import uploads
+    return uploads.update_all_channels()
