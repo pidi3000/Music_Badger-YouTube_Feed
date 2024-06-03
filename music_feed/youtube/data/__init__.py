3d0c501fe82a719c06beae17f684a24aac8a7748
pidi3000 <pidi3000@live.com>
2024-06-03 19:33:47 +0200

    fix(`youtube/data`): minor restructuring of `uploads` import

--- music_feed/youtube/data/__init__.py
+++ music_feed/youtube/data/__init__.py
@@ -1,8 +1,4 @@
 
 from .subscriptions import *
 from .channel import *
-
-
-def update_Uploads():
-    from music_feed.youtube.data import uploads
-    return uploads.update_all_channels()
+from .uploads import *
